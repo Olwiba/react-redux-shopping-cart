c730027cb94da566386f27bfd3b75e889c6ac514
Ollie Bannister <olwiba@gmail.com>
2016-07-12 11:25:08 +1200

    Completed the remove item functionality

--- app/components/product-detail.jsx
+++ app/components/product-detail.jsx
@@ -7,14 +7,22 @@ class ProductDetail extends Component {
     const {id} = this.props.params
     const product = this.props.products.filter(p => p.get('id') == id ).first()
     const name = product.get('name')
+
     const addToCart = (e) => {
       e.preventDefault()
       this.props.addToCart(id)
     }
+
+    const removeFromCart = (e) => {
+      e.preventDefault()
+      this.props.removeFromCart(id)
+    }
+
     return (
       <div className='product' id={id}>
         <div> Detailed view of a product {name}</div>
         <div><a href='' onClick={addToCart}>Add to Cart</a></div>
+        <div><a href='' onClick={removeFromCart}>Remove from Cart</a></div>
         <div><Link to='/'>View all</Link></div>
       </div>
     )
@@ -32,6 +40,12 @@ function mapDispatchToProps(dispatch) {
     addToCart: (id) => { 
       dispatch({
         type: 'ADD_PRODUCT_TO_CART',
+        id: parseInt(id)
+      })
+    },
+    removeFromCart: (id) => { 
+      dispatch({
+        type: 'REMOVE_PRODUCT_FROM_CART',
         id: parseInt(id)
       })
     }
--- app/reducer.js
+++ app/reducer.js
@@ -15,6 +15,10 @@ export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'ADD_PRODUCT_TO_CART':
       return state.set('cart', state.get('cart').push(action.id))
+    case 'REMOVE_PRODUCT_FROM_CART':
+      const index = state.get('cart').indexOf(action.id)
+      console.log('this is the index: ', index)
+      return state.set('cart', state.get('cart').splice(index, 1))
     default:
       return state
   }
